10fd92d945a15960c6b33f39403d59548ab196a4
h3ni <djebbiheni01@gmail.com>
2023-12-22 12:13:46 +0100

    api project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) =>
+        // handle success
+        {
+          setUser(response.data);
+          console.log(response);
+        }
+      )
+      .catch((error) =>
+        // handle error
+        console.log(error)
+      );
+  }, []);
+
+  const handleChange = (e) =>
+    setUser({ id: Date.now(), [e.target.name]: e.target.value });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Person Name:
+          <input type="text" name="name" onChange={handleChange} />
+        </label>
+        <button type="submit">Add</button>
+      </form>
+      <div>
+        {user.map((user) => {
+          return <h>{user.name} | </h>;
+        })}
+      </div>
     </div>
   );
-}
-
+};
 export default App;
